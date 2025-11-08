f3eb562117f930a5e5080f5df73c65f005acbbc9
pradumay4545 <patelpradhumn028@gmail.com>
2025-11-08 13:39:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function lightTheme(){
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+}
+
+function darkTheme(){
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+}
